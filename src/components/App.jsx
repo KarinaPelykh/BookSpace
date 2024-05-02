deccbefe72b0906da50cba24a0524226512e6a28
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-05-02 21:38:41 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-import { HomePage } from '../pages/HomePage/HomePage';
+// import { HomePage } from '../pages/HomePage/HomePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
+        {/* <Route index element={<HomePage />} /> */}
         <Route path="*" element={<h1>Not found</h1>} />
       </Route>
     </Routes>
