22f36b6ffd65bda6888fd31b10e8b11d6efdcd23
Olekasndr Gubskiy <infernokgg@gmail.com>
2024-05-03 18:15:49 +0300

    test suspense

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import { NavLink, Outlet } from 'react-router-dom';
 import Header from '../../modules/Header/Header__box';
 import Footer from '../../modules/Footer/Footer';
 import { HomePage } from '../../pages/HomePage/HomePage';
+import { Suspense } from 'react';
 export const Layout = () => {
   return (
     <>
@@ -10,8 +11,12 @@ export const Layout = () => {
       <main>
         <Outlet />
       </main> */}
-      <HomePage />
-
+      {/* <HomePage /> */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <main>
+          <Outlet />
+        </main>
+      </Suspense>
       <Footer />
     </>
   );
